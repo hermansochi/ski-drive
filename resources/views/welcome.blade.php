be402a6f7ca00aef789ee392277cb21206cec9b2
Herman Alexandrov <hermansochi@ya.ru>
2023-02-28 15:06:03 +0300

    FIX yandex metrica

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,24 +9,6 @@
         <link type="image/png" rel="icon" href="{{ Vite::asset('resources/img/64.png') }}">
         <meta name="description" content="Прокат лыж, сноубордов, полной экипировки на горнолыжных курортах Сочи. У нас можно взять в прокат лыжи, сноуборд">
         <title>Прокат горнолыжного снаряжения Красная поляна</title>
-
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript" >
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-        ym(92623638, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true
-        });
-    </script>
-    <noscript><div><img src="https://mc.yandex.ru/watch/92623638" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-    <!-- /Yandex.Metrika counter -->
     </head>
 
 <body>
@@ -449,7 +431,25 @@
         <button class="modal__close modal__exit">&times;</button>
     </div>
 </div>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(92623638, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+    });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/92623638" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&apikey=b16d2be2-a9a0-4c24-b977-b135f92dcfe8"></script>
+
 </body>
 
 </html>
